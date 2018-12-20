ba0f05178d8e86ba4cc39401a50f14e3e5cc00d9
kaikecarlos <caheasta@gmail.com>
2018-12-20 16:27:45 -0300

    Register view

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/register', (req, res, next) => {
+  res.render('user/register')
+});
 
 module.exports = router;
