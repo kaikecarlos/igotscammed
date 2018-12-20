9cdf4b9022ed820175226e279695ad8be6d3351f
kaikecarlos <caheasta@gmail.com>
2018-12-20 16:43:15 -0300

    :sparkles: Add Login View

--- app.js
+++ app.js
@@ -20,9 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressLayouts);
-
+app.set("layout extractScripts", true);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
